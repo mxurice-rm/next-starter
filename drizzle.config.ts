1f4cec1c73c99bedadeaa53a2ba744ca4420f1f0
mxurice-rm <reim@interaktiv.de>
2025-11-04 22:24:56 +0100

    chore(project): fix env variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   dbCredentials: {
     host: process.env.POSTGRES_HOST || 'next-database',
     port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-    database: process.env.PG_DATABASE || 'next',
+    database: process.env.POSTGRES_DATABASE || 'next',
     user: process.env.POSTGRES_USER || 'next',
     password: process.env.POSTGRES_PASSWORD || 'next',
     ssl: 'allow',
